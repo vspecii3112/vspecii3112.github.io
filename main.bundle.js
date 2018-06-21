87b9e9710252a77c1f0690b4df7d1a4547f5e486
vspecii3112 <kennyklleung@gmail.com>
2018-06-21 16:25:44 -0400

    currency api

--- main.bundle.js
+++ main.bundle.js
@@ -1812,7 +1812,7 @@ var currency = /** @class */ (function () {
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return DOMAIN; });
 var DOMAIN = {
-    url: "http://localhost:3333" // http://localhost:3333 , https://cstoreapi.herokuapp.com;
+    url: "https://cstoreapi.herokuapp.com" // http://localhost:3333 , https://cstoreapi.herokuapp.com;
 };
 
 
